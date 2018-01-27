871ff318000213fffc84574cc9013106c6c3d3ba
Lucian Popescu <lucian.gabriel.popescu@gmail.com>
2018-01-27 10:49:51 +0200

    removed unnecessary files

--- tests/bootstrap.php
+++
@@ -1,7 +0,0 @@
-<?php
-function getSourceFileName($strTestFileName) {
-	$intPosition = strrpos($strTestFileName,"/tests/");
-	$strCommonFolder = substr($strTestFileName, 0, $intPosition);
-	$strTestPath = str_replace(array("/tests/","Test.php"),array("/src/",".php"), substr($strTestFileName, $intPosition));
-	return $strCommonFolder.$strTestPath;
-}
