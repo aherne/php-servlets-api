a344f5eae4b9bb9ddbf5fa26b9846baaced1fe65
Lucian Gabriel Popescu <lucian.gabriel.popescu@gmail.com>
2026-04-07 06:30:21 +0300

    php8.5 compatibility

--- test.php
+++ test.php
@@ -1,7 +1,8 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
 try {
-    new Lucinda\UnitTest\ConsoleController("unit-tests.xml", "local");
-} catch (Exception $e) {
-    echo $e->getMessage();
+    new Lucinda\UnitTest\ConsoleController("unit-tests.xml", "local", ($argv[1] ?? null));
+} catch (\Throwable $e) {
+    echo "ERROR: ".$e->getMessage().PHP_EOL;
+    echo "TRACE: ".$e->getTraceAsString();
 }
